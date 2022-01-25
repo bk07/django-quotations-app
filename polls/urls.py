e2d4709f944d6eaba18ae38007cddd8e7e04459e
harishchennupati2 <harishchennupati2@gmail.com>
2022-01-25 12:53:03 +0530

    init

---
+++ polls/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
